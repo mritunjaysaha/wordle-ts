cabf74ba27403e21703f00951bbbebf3fa1ee725
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-02-19 11:55:26 +0530

    feat: updated footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,19 @@
+import { ReactComponent as IconGitHub } from '../../assets/github.svg';
+
 export const Footer = () => {
     return (
         <footer className='bg-violet-dark p-2 text-center text-white'>
-            <p>
-                Made by <a href='https://github.com/mritunjaysaha'>Mritunjay</a>
+            <p className='flex items-center justify-center'>
+                Made by&nbsp;
+                <a
+                    className='flex items-center justify-center'
+                    href='https://github.com/mritunjaysaha'
+                >
+                    Mritunjay&nbsp;
+                    <span className='text-white-500 hover:underline'>
+                        <IconGitHub className='fill-current h-5 w-5' />
+                    </span>
+                </a>
             </p>
         </footer>
     );
