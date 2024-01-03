41a20688ab9f17db0194a0e8b69f09cdb8077ffe
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-03 23:53:47 +0530

    feat: added leader board page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { ROUTES } from './constants/routes';
 import Home from './Pages/Home';
+import LeaderBoard from './Pages/LeaderBoard';
 import LogIn from './Pages/LogIn';
 import SignUp from './Pages/SignUp';
 
@@ -11,6 +12,7 @@ export const AppRoutes = () => {
             <Route path={ROUTES.HOME} element={<Home />} />
             <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
             <Route path={ROUTES.LOG_IN} element={<LogIn />} />
+            <Route path={ROUTES.LEADER_BOARD} element={<LeaderBoard />} />
         </Routes>
     );
 };
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,4 +2,5 @@ export const ROUTES = {
     HOME: '/',
     SIGN_UP: '/auth/signup',
     LOG_IN: '/auth/login',
+    LEADER_BOARD: '/leader-board',
 };
