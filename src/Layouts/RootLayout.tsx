9d0922c4194a1091462093686180c02594cf37ae
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-06 00:19:37 +0530

    feat: added outside click handler for leader board

--- src/Layouts/RootLayout.tsx
+++ src/Layouts/RootLayout.tsx
@@ -1,16 +1,21 @@
 import type { ReactNode } from 'react';
 import { Toaster } from 'react-hot-toast';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 import { useAuthContext } from '../hooks/useAuthContext';
 import LeaderBoard from '../Pages/LeaderBoard';
 
 export const RootLayout = ({ children }: { children: ReactNode }) => {
-    const { openLeaderBoard } = useAuthContext();
+    const { openLeaderBoard, setOpenLeaderBoard } = useAuthContext();
 
     return (
         <section className='flex h-screen flex-col'>
             {children}
-            {openLeaderBoard && <LeaderBoard />}
+            {openLeaderBoard && (
+                <OutsideClickHandler onOutsideClick={() => setOpenLeaderBoard(false)}>
+                    <LeaderBoard />
+                </OutsideClickHandler>
+            )}
             <Toaster />
         </section>
     );
