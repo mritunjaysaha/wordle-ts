5e2483690ab948ac4f0ecab0118f226683b0f650
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-13 19:43:23 +0530

    chore: removed console logs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import removeConsole from 'vite-plugin-remove-console';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react()],
+    plugins: [svgr(), react(), removeConsole()],
 });
